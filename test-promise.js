e133c97086d08170c94188705a460b376b22cff8
czhen <cheng.zhen8387@gmail.com>
2021-03-22 01:37:56 +0100

    add promise creation example v1

--- test-promise.js
+++ test-promise.js
@@ -1,6 +1,40 @@
 // Promise创建
+function myPromise(constructor) {
+    let self = this;
+    self.status = "pending";
+    self.value = undefined;
+    self.reason = undefined;
+    function resolve(value) {
+        if (self.status === "pending") {
+            self.value = value;
+            self.status = "resolved";
+        }
+    }
+    function reject(reason) {
+        if (self.status === "pending") {
+            self.reason = reason;
+            self.status = "rejected";
+        }
+    }
+    try {
+        constructor(resolve, reject);
+    } catch (e) {
+        reject(e);
+    }
+}
 
-
+myPromise.prototype.then = function (onFulFilled, onRejected) {
+    let self = this;
+    switch (self.status) {
+        case "resolved" :
+            onFulFilled(self.value);
+            break;
+        case "rejected" :
+            onRejected(self.reason);
+            break;
+        default :
+    }
+}
 
 // Promise应用
 
@@ -17,7 +51,7 @@ var fn = function() {
     }, 5000);
 }
 
-var studentPromise = new Promise((resolve, reject) => {
+var studentPromise = new myPromise((resolve, reject) => {
     if(student.isGraduated) {
         resolve(student);
     } else {
@@ -29,8 +63,4 @@ fn();
 
 studentPromise.then((student) => {
     console.log(student.name);
-}).catch((e) => {
-    console.log(e);
-}).finally(() => {
-    console.log("done");
-})
+});
